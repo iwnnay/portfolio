ec91fad0dab66345b439503f64082eff0c98d09a
Joe Imhoff <tech@flexshot.site>
2026-01-26 19:27:33 -0600

    There's some error and I can't find it.

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,3 +1,56 @@
-window.addEventListener('load', () => {
+let openSkillDialog;
+document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.passive-logo .circle').addEventListener('click', () => document.location = '/');
+
+    const slugName = (name) => name.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-+|-+$/g, '');
+
+    const durations = document.getElementsByClassName('duration');
+    const starts = document.getElementsByClassName('start');
+    const ends = document.getElementsByClassName('end');
+
+    if (durations.length === starts.length && durations.length === ends.length) {
+        for (let i = 0; i < durations.length; i++) {
+            const startDate = new Date(starts[i].innerText);
+            const endDate = ends[i].innerText === 'Present' ? new Date() : new Date(ends[i].innerText);
+            const duration = durations[i];
+
+            let years = endDate.getFullYear() - startDate.getFullYear();
+            let months;
+            if (startDate.getMonth() < endDate.getMonth()) {
+                months = endDate.getMonth() - startDate.getMonth()
+            } else {
+                months = 12 - startDate.getMonth() + endDate.getMonth();
+                years -= 1;
+            }
+
+            duration.innerText = (years ? `${years} year${years > 1 ? 's' : ''} ` : '')
+                + (months ? `${months} month${months > 1 ? 's' : ''} ` : '')
+
+        }
+    }
+
+    const closeSkillDialog = () => {
+        if (openSkillDialog) {
+            openSkillDialog.close();
+        }
+    }
+
+    for (let skillEl of document.getElementsByClassName('skill')) {
+        const skill = skillEl.innerText;
+        const slug = slugName(skill);
+        if (!document.getElementById(slugName('skill-' + slug))) {
+            console.log('need to add skill detail for ' + skill);
+        }
+
+        skillEl.addEventListener('click', () => {
+            closeSkillDialog();
+
+            openSkillDialog = document.getElementById(slugName('skill-' + slug));
+            openSkillDialog.showModal();
+        });
+    }
+
+    for (let closeBtn of document.getElementsByClassName('close-skill')) {
+        closeBtn.addEventListener('click', () => closeSkillDialog());
+    }
 });
--- resume/index.md
+++ resume/index.md
@@ -2,18 +2,45 @@
 layout: base
 title: Resume
 ---
+
 # Joe Imhoff
 Contact me through my socials [LinkedIn](https://www.linkedin.com/in/joe-imhoff/) | [GitHub](https://github.com/iwnnay)
 
 {% for job in site.data.resume.jobs %}
-## {{ job.company }}
-**{{ job.location }}**
+<div class="company-name">{{ job.company }}</div>
+<div class="company-info">
+<div class="company-location">{{ job.location }}</div>
+{% if job.start and job.end %}
+    <div class="job-duration">
+    <span class="start">{{ job.start }}</span> - <span class="end">{{ job.end }}</span> <span class="duration"></span>
+    </div>
+{%  endif %}
+</div>
     {% for position in job.positions %}
-### {{ position.title }}
-{{ position.start }} - {{ position.end }}
+<div class="job-title">{{ position.title }}</div>
+<div class="job-duration">
+<span class="start">{{ position.start }}</span> - <span class="end">{{ position.end }}</span> <span class="duration"></span>
+</div>
 
+<ul>
     {% for responsibility in position.responsibilities %}
-- {{ responsibility }}
+    <li>{{ responsibility }}</li>
     {% endfor %}
+</ul>
+<div class="skills">
+{% for skill in position.skills %}
+    <span class="skill">{{ skill }}</span>
 {% endfor %}
+</div>
+{% endfor %}
+{% endfor %}
+
+{% for skill in site.data.resume.skills %}
+<dialog id="skill-{{ skill.name | slugify }}">
+    <h3>{{ skill.name }}</h3>
+    <div class="button close-skill">X</div>
+    <div class="skill-level"><span class="skill-label">Level:</span> <span class="skill-level-value">{{ skill.level }}</span></div>
+    <div class="skill-years"><span class="skill-label">Years:</span> <span class="skill-years-value">{{ skill.years }}</span></div>
+    <div class="skill.details">{{ skill.details }}</div>
+</dialog>
 {% endfor %}
